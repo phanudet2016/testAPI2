421c23a5168f5454903d46eba4aca171af3c6691
phanudet2016 <panudach_beer_2012@hotmail.co.th>
2019-01-10 09:28:03 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+var users = require('./users');
+
+app.use(cors())
+
+var port = process.env.PORT || 7778;
+
+app.get('/', function (req, res) {
+    res.send('<h1>Hello Node.js</h1>');
+});
+
+app.get('/user', function (req, res) {
+    res.json(users.findAll());
+});
+
+app.get('/user/:id', function (req, res) {
+    var id = req.params.id;
+    res.json(users.findById(id));
+});
+
+app.post('/newuser', function (req, res) {
+    var json = req.body;
+    res.send('Add new ' + json.name + ' Completed!');
+});
+
+
+app.listen(port, function() {
+    console.log('Starting node.js on port ' + port);
+});
